c1c4a374560eb2aa108d976c47f49d43f7de67ad
biasanchez <beatrizsanchezribeiro07@gmail.com>
2024-11-29 14:47:22 -0300

    Add files via upload

---
+++ respostas.js
@@ -0,0 +1,302 @@
+criaCartao(
+    'Tabela Periódica',
+    'O que é a tabela periódica?',
+    'A tabela periódica é uma organização dos elementos químicos, dispostos de acordo com suas propriedades e número atômico.'
+)
+
+criaCartao(
+    'Átomo',
+    'O que é um átomo?',
+    'Um átomo é a menor unidade de um elemento químico que mantém as propriedades desse elemento.'
+)
+
+criaCartao(
+    'Molecula',
+    'O que é uma molécula?',
+    'Uma molécula é uma combinação de dois ou mais átomos que formam uma unidade estável.'
+)
+
+criaCartao(
+    'Substâncias',
+    'Qual a diferença entre substância pura e mistura?',
+    'Substância pura tem composição uniforme e propriedades definidas, enquanto mistura é a combinação de duas ou mais substâncias.'
+)
+
+criaCartao(
+    'Reação Química',
+    'O que caracteriza uma reação química?',
+    'Uma reação química ocorre quando substâncias se transformam em outras substâncias, mudando suas propriedades.'
+)
+
+criaCartao(
+    'pH',
+    'O que é pH?',
+    'pH é uma medida da acidez ou basicidade de uma solução, que varia de 0 a 14.'
+)
+
+criaCartao(
+    'Ligação Iônica',
+    'O que é uma ligação iônica?',
+    'Uma ligação iônica é a força eletrostática que une íons de cargas opostas, criando compostos iônicos.'
+)
+
+criaCartao(
+    'Ligação Covalente',
+    'O que é uma ligação covalente?',
+    'Uma ligação covalente é quando dois átomos compartilham pares de elétrons para formar uma molécula.'
+)
+
+criaCartao(
+    'Isótopos',
+    'O que são isótopos?',
+    'Isótopos são átomos de um mesmo elemento que têm o mesmo número de prótons, mas diferentes números de nêutrons.'
+)
+
+criaCartao(
+    'Soluções',
+    'O que é uma solução?',
+    'Uma solução é uma mistura homogênea de duas ou mais substâncias, onde uma delas é o soluto e a outra o solvente.'
+)
+
+criaCartao(
+    'Ácido',
+    'O que é um ácido?',
+    'Um ácido é uma substância que libera íons de hidrogênio (H+) em solução aquosa.'
+)
+
+criaCartao(
+    'Base',
+    'O que é uma base?',
+    'Uma base é uma substância que aceita íons de hidrogênio ou libera íons hidróxido (OH-) em solução.'
+)
+
+criaCartao(
+    'Estado Físico',
+    'Quais são os principais estados físicos da matéria?',
+    'Os principais estados físicos são sólido, líquido e gasoso.'
+)
+
+criaCartao(
+    'Estequiometria',
+    'O que é estequiometria?',
+    'Estequiometria é o cálculo das quantidades relativas de reagentes e produtos em uma reação química.'
+)
+
+criaCartao(
+    'Covalente Polar',
+    'O que é uma ligação covalente polar?',
+    'Uma ligação covalente polar ocorre quando há desigualdade na distribuição de elétrons entre os átomos, resultando em um dipolo elétrico.'
+)
+
+criaCartao(
+    'Covalente Não Polar',
+    'O que é uma ligação covalente não polar?',
+    'Uma ligação covalente não polar ocorre quando os elétrons são compartilhados igualmente entre átomos de mesmos elementos.'
+)
+
+criaCartao(
+    'Energia de Ativação',
+    'O que é energia de ativação?',
+    'Energia de ativação é a energia mínima necessária para que uma reação química ocorra.'
+)
+
+criaCartao(
+    'Termodinâmica',
+    'O que estuda a termodinâmica?',
+    'A termodinâmica estuda as relações entre calor, trabalho, temperatura e energia em sistemas químicos.'
+)
+
+criaCartao(
+    'Oxidação e Redução',
+    'Qual a diferença entre oxidação e redução?',
+    'Oxidação é a perda de elétrons, enquanto redução é o ganho de elétrons por uma substância.'
+)
+
+criaCartao(
+    'Catalisadores',
+    'O que são catalisadores?',
+    'Catalisadores são substâncias que aumentam a velocidade de uma reação química sem serem consumidas.'
+)
+
+criaCartao(
+    'Íons',
+    'O que são íons?',
+    'Íons são átomos ou moléculas que possuem carga elétrica devido à perda ou ganho de elétrons.'
+)
+
+criaCartao(
+    'Estado de Agitação',
+    'O que é estado de agitação molecular?',
+    'É a movimentação dos átomos e moléculas em um sistema, que aumenta com a temperatura.'
+)
+
+criaCartao(
+    'Reações de Combustão',
+    'O que são reações de combustão?',
+    'Reações de combustão ocorrem quando uma substância reage rapidamente com oxigênio, liberando energia na forma de calor e luz.'
+)
+
+criaCartao(
+    'Água',
+    'Qual a importância da água na química?',
+    'A água é um solvente universal e desempenha um papel crucial nas reações químicas biológicas e fisiológicas.'
+)
+
+criaCartao(
+    'Sais',
+    'O que são sais?',
+    'Sais são compostos formados pela combinação de um ácido e uma base, resultando na neutralização dos respectivos íons.'
+)
+
+criaCartao(
+    'Equilíbrio Químico',
+    'O que é equilíbrio químico?',
+    'Equilíbrio químico ocorre quando as taxas de reação direta e reversa são iguais.'
+)
+
+criaCartao(
+    'Polímeros',
+    'O que são polímeros?',
+    'Polímeros são macromoléculas formadas por unidades repetitivas chamadas monômeros.'
+)
+
+criaCartao(
+    'Reação de Precipitação',
+    'O que é uma reação de precipitação?',
+    'Uma reação de precipitação ocorre quando dois reagentes se combinam para formar um sólido insolúvel.'
+)
+
+criaCartao(
+    'Entalpia',
+    'O que é entalpia?',
+    'Entalpia é a quantidade total de energia de um sistema, incluindo energia interna e trabalho feito sobre ou pelo sistema.'
+)
+
+criaCartao(
+    'Leis dos Gases',
+    'Quais são as leis dos gases ideais?',
+    'As leis dos gases ideais incluem a Lei de Boyle, a Lei de Charles, e a Lei de Avogadro que relacionam pressão, volume, e temperatura.'
+)
+
+criaCartao(
+    'Reações Exotérmicas',
+    'O que são reações exotérmicas?',
+    'Reações exotérmicas são aquelas que liberam energia, geralmente na forma de calor, durante o decorrer da reação.'
+)
+
+criaCartao(
+    'Reações Endotérmicas',
+    'O que são reações endotérmicas?',
+    'Reações endotérmicas são aquelas que absorvem energia do ambiente durante o processo reacional.'
+)
+
+criaCartao(
+    'Número Atômico',
+    'O que representa o número atômico de um elemento?',
+    'O número atômico representa o número de prótons no núcleo de um átomo e determina a identidade do elemento.'
+)
+
+criaCartao(
+    'Propriedades Periódicas',
+    'O que são propriedades periódicas?',
+    'Propriedades periódicas são características que variam de acordo com a posição dos elementos na tabela periódica, como eletronegatividade e raio atômico.'
+)
+
+criaCartao(
+    'Hibridização',
+    'O que é hibridização na química?',
+    'Hibridização é o conceito de combinar orbitais atômicos para formar novos orbitais híbridos que determinam a geometria molecular.'
+)
+
+criaCartao(
+    'Reações de Deslocamento',
+    'O que são reações de deslocamento?',
+    'Reações de deslocamento envolvem a troca de íons entre compostos, onde um elemento desloca outro.'
+)
+
+criaCartao(
+    'Condutividade Elétrica',
+    'Qual a relação entre ionização e condutividade elétrica?',
+    'A condutividade elétrica em soluções ocorre devido à presença de íons que se movem livremente.'
+)
+
+criaCartao(
+    'Misturas Coloidais',
+    'O que são misturas coloidais?',
+    'Misturas coloidais são misturas em que pequenas partículas de uma substância estão dispersas em outra, sem se dissolverem completamente.'
+)
+
+criaCartao(
+    'Fases da Matéria',
+    'Quais são as fases da matéria?',
+    'As principais fases da matéria são sólido, líquido, gasoso e plasma.'
+)
+
+criaCartao(
+    'Nomenclatura Química',
+    'O que é nomenclatura química?',
+    'Nomenclatura química é um sistema de nomes usados para identificar compostos químicos baseados em suas estruturas e compostições.'
+)
+
+criaCartao(
+    'Reações Redox',
+    'O que caracteriza uma reação redox?',
+    'Reações redox envolvem a transferência de elétrons entre substâncias, incluindo oxidação e redução.'
+)
+
+criaCartao(
+    'Solubilidade',
+    'O que é solubilidade?',
+    'Solubilidade é a quantidade máxima de soluto que pode se dissolver em um dado volume de solvente a uma temperatura específica.'
+)
+
+criaCartao(
+    'Ácidos Fortes e Fracos',
+    'Qual a diferença entre ácidos fortes e fracos?',
+    'Ácidos fortes ionizam completamente em solução, enquanto ácidos fracos ionizam parcialmente.'
+)
+
+criaCartao(
+    'Bases Fortes e Fracas',
+    'Qual a diferença entre bases fortes e fracas?',
+    'Bases fortes ionizam completamente em solução, enquanto bases fracas ionizam parcialmente.'
+)
+
+criaCartao(
+    'Polímeros Naturais',
+    'Quais são exemplos de polímeros naturais?',
+    'Exemplos de polímeros naturais incluem celulose, proteínas e DNA.'
+)
+
+criaCartao(
+    'Tensão Superficial',
+    'O que é tensão superficial?',
+    'Tensão superficial é a propriedade da superfície de um líquido que faz com que ela se comporte como uma película elástica.'
+)
+
+criaCartao(
+    'Análise Qualitativa',
+    'O que é análise qualitativa?',
+    'Análise qualitativa é a determinação da composição de uma substância, identificando os elementos ou compostos contidos.'
+)
+
+criaCartao(
+    'Análise Quantitativa',
+    'O que é análise quantitativa?',
+    'Análise quantitativa é a determinação da quantidade ou concentração de uma substância específica em uma amostra.'
+)
+
+criaCartao(
+    'Cadeias Carbônicas',
+    'O que são cadeias carbônicas?',
+    'Cadeias carbônicas são estruturas formadas por átomos de carbono que podem ser lineares ou ramificadas e podem ser abertas ou fechadas.'
+)
+
+criaCartao(
+    'Condutância',
+    'O que é condutância na química?',
+    'Condutância é a medida da capacidade de uma solução de conduzir eletricidade, que depende da concentração de íons presentes.'
+)
+
+
+
